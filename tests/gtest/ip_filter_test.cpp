e6f149784c4414463eca5cf2656c0ade7c4390a3
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-03-08 15:03:01 +0300

    Настроено автоматическое обнаружение тестов GoogleTest и добавлены тесты split

--- tests/gtest/ip_filter_test.cpp
+++ tests/gtest/ip_filter_test.cpp
@@ -12,3 +12,29 @@ TEST(SplitTest, BasicSplit) {
     EXPECT_EQ(result[2], "33");
     EXPECT_EQ(result[3], "44");
 }
+// ("", '.') -> [""]
+
+TEST(SplitTest, EmptyString) {
+    auto result = split("", '.');
+
+    ASSERT_EQ(result.size(), 1);
+    EXPECT_EQ(result[0], "");
+}
+
+// ("11.", '.') -> ["11", ""]
+TEST(SplitTest, TrailingDelimiter) {
+    auto result = split("11.", '.');
+
+    ASSERT_EQ(result.size(), 2);
+    EXPECT_EQ(result[0], "11");
+    EXPECT_EQ(result[1], "");
+}
+
+// (".11", '.') -> ["", "11"]
+TEST(SplitTest, LeadingDelimiter) {
+    auto result = split(".11", '.');
+
+    ASSERT_EQ(result.size(), 2);
+    EXPECT_EQ(result[0], "");
+    EXPECT_EQ(result[1], "11");
+}
