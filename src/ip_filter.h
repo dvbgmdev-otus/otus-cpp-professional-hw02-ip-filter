6c0ed5b61d309881bd42cfe9c5def2eaecd5349c
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-03-08 11:41:35 +0300

    Выделена библиотека ip_filter и перенесена функция split

---
+++ src/ip_filter.h
@@ -0,0 +1,9 @@
+#ifndef IP_FILTER_H
+#define IP_FILTER_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(const std::string &str, char d);
+
+#endif // IP_FILTER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "ip_filter.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
